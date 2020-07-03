ae7cc29855a391da824ba7c7475b2c470a726aed
reid <reidarwood@gmail.com>
2020-07-02 21:34:33 -0400

    i think this one is a botch

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
+from django.conf.urls import url
+from mysite.core import views as core_views
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.add_recipe, name="create"),
     path('<int:recipe_id>', views.recipe_detail, name='detail'),
-    
+    url(r'^signup/$', core_views.signup, name='signup'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from .models import Recipe
 from .forms import RecipeForm, InstructionForm, IngredientForm
 from django.forms.formsets import formset_factory
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'login.html', {'form': form})
 
 # Create your views here.
 def index(request):
